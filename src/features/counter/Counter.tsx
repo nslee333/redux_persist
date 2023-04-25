6bbc8a08a3011ea0117d194b488896fe3996d3aa
Nathan <nslee333@gmail.com>
2023-04-25 07:58:51 -0700

    Formatting.

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import type {RootState} from "../../app/store";
 import { useSelector, useDispatch } from "react-redux";
-import {decrement, increment} from "./counterSlice";
+import { decrement, increment } from "./counterSlice";
 
 export function Counter() {
   const count = useSelector((state: RootState) => state.counter.value)
   const dispatch = useDispatch();
 
+  // & Get redux persist working 
+
   return (
     <div>
       <div>
         <button
-        aria-label="increment value"
-        onClick={() => dispatch(increment())}
+          aria-label="increment value"
+          onClick={() => dispatch(increment())}
         >
           Increment
         </button>
       <span>{count}</span>
         <button
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
         >
           Decrement
         </button>
+      
       </div>
     </div>
   );
