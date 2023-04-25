59ad47e8a764be256c6e4b36215ffd42bc0daef3
Nathan <nslee333@gmail.com>
2023-04-25 07:59:09 -0700

    WIP: Integrating redux-persist in store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice"
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, counterReducer);
+
+
 
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: persistedReducer,
   }
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
